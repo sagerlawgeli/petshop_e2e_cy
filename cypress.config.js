d416ec27ebfc9daadf374cc547b7feaaa21c0a88
Hemi <sagerlawgeli@gmail.com>
2023-04-21 17:57:43 +0100

    Adjustments to config and env handling

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,31 @@
 const { defineConfig } = require("cypress");
+const { readFileSync } = require("fs");
 
 module.exports = defineConfig({
   e2e: {
+    // Implement node event listeners here
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+
+      // Check if an environment name has been specified in the configuration
+      if (config.env.name !== undefined) {
+
+        // If an environment name has been specified, load the corresponding environment file
+        const envName = config.env.name;
+        const envFile = readFileSync(
+          "cypress/config/cypress." + envName + ".json"
+        );
+
+        // Parse the environment file as JSON and assign its properties to the configuration object
+        const envValues = JSON.parse(envFile);
+        config.env = {
+          ...envValues,
+        };
+      }
+
+      // Return the updated configuration object
+      return config;
     },
+    video: false,
+    watchForFileChanges: false,
   },
 });
