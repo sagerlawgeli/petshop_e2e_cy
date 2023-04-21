f0ef2fc8199c90c22869ff77f272b384ea5b3fe3
Hemi <sagerlawgeli@gmail.com>
2023-04-21 19:32:15 +0100

    Adds a test case to ensure orders are listed correctly on the dashboard

---
+++ cypress/e2e/admin/dashboard.cy.js
@@ -0,0 +1,30 @@
+/// <reference types='Cypress' />
+const env = Cypress.env();
+
+describe('dmin: Running tests on the dsahboard (orders) page', { testIsolation: false }, () => {
+    before(() => {
+        // Clear saved sessions and login as admin
+        Cypress.session.clearAllSavedSessions()
+        cy.adminLoginUI()
+    });
+
+
+    describe('Test sales orders', () => {
+        it('Should create a new order', () => {
+            // Intercept the API request that fetchs orders
+            cy.intercept(env.baseUrlAPI + 'orders/dashboard*').as('fetchRequest')
+
+            // Create a new order
+            cy.createSales().then((uuid) => {
+                // Navigate to today's orders on the dashboard
+                cy.get('.page-header span').contains('Today').click();
+
+                // Wait for the intercepted request to complete and check the response status code
+                cy.wait('@fetchRequest').its('response.statusCode').should('eq', 200)
+
+                // Check if the new order's ID is displayed in the orders table
+                cy.get('td').should('include.text', uuid);
+            })
+        });
+    });
+});
