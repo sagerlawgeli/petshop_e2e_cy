776aa1827e74d020ac12a2f141d4398c70abff60
Hemi <sagerlawgeli@gmail.com>
2023-04-21 18:17:44 +0100

    import UI commands file

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,5 +1,6 @@
-// Import API commands
+// Import API & UI commands
 import * as apiCmd from './api.cmd.js';
+import * as uiCmd from './api.cmd.js';
 
 // Authentication
 
@@ -7,4 +8,10 @@ Cypress.Commands.add('adminLoginAPI', () => {
     Cypress.log({ name: 'adminLoginAPI' });
 
     return apiCmd.adminLoginAPI();
+});
+
+Cypress.Commands.add('adminLoginUI', () => {
+    Cypress.log({ name: 'adminLoginUI' });
+
+    return uiCmd.adminLoginUI();
 });
